2c26bfaa4864cb5b170a1ca7dac257ead5443c0d
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-02-02 11:26:06 +0300

    hot-fix: переименовать pop-up в popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,39 @@
 function openPopUp() {
   let name = document.querySelector('.profile__name').textContent;
-  let nameInput = document.querySelector('.pop-up__input-name');
+  let nameInput = document.querySelector('.popup__input-name');
   let job = document.querySelector('.profile__profession').textContent;
-  let jobInput = document.querySelector('.pop-up__input-profession');
-  let popUp = document.querySelector('.pop-up');
+  let jobInput = document.querySelector('.popup__input-profession');
+  let popUp = document.querySelector('.popup');
   
-  popUp.classList.add('pop-up_opened');
+  popUp.classList.add('popup_opened');
   nameInput.value = name;
   jobInput.value = job;
 }
 
 
 function closePopUp() {
-  let popUp = document.querySelector('.pop-up');
-  popUp.classList.remove('pop-up_opened');
+  let popUp = document.querySelector('.popup');
+  popUp.classList.remove('popup_opened');
 }
 
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector('.pop-up__input-name').value;
-  let jobInput = document.querySelector('.pop-up__input-profession').value;
+  let nameInput = document.querySelector('.popup__input-name').value;
+  let jobInput = document.querySelector('.popup__input-profession').value;
   document.querySelector('.profile__name').textContent = nameInput;
   document.querySelector('.profile__profession').textContent = jobInput;
 
-  let popUp = document.querySelector('.pop-up');
-  popUp.classList.remove('pop-up_opened');
+  let popUp = document.querySelector('.popup');
+  popUp.classList.remove('popup_opened');
 }
 
 
 let btnOpen = document.querySelector('.profile__edit-button');
 btnOpen.addEventListener('click', openPopUp);
 
-let btnClose = document.querySelector('.pop-up__button-close');
+let btnClose = document.querySelector('.popup__button-close');
 btnClose.addEventListener('click', closePopUp);
 
-let formElement = document.querySelector('.pop-up__container');
+let formElement = document.querySelector('.popup__container');
 formElement.addEventListener('submit', handleFormSubmit);
